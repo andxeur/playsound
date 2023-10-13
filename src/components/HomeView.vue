da960833c1df2b855d125fc48a08c4f849443305
andxeur <andxeur@gmail.com>
2023-10-13 21:19:13 +0000

    struturation du projet et ajouts des icon

---
+++ src/components/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="container-fluid bg-black vh-100 w-100 p-0">
+
+    <div class="top-app row bg-success m-0" style="height: 90%; border-bottom-left-radius: 48px; border-bottom-right-radius: 48px;">
+      <div class="top-app-left col-2 bg-grey-dark" style="border-bottom-left-radius: 48px;">
+      </div>
+      <div class="top-app-right col-10 bg-white" style="border-bottom-right-radius: 48px;">
+      </div>
+    </div>
+
+    <div class="bottom-app container-fluid bg-black" style="height: 10%">
+    </div>
+
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
